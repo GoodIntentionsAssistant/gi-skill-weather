8f2ce07574ee72110bb6d4fc7691efa958b86d6b
Darren Moore <darren.m@firecreekweb.com>
2018-11-10 14:44:40 +0700

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Good Intentions Skill Weather
+
+Weather skill for Good Intentions Assistant
+
+## Documentation
+
+http://goodintentions.firecreekweb.com
---
+++ weather_skill.js
@@ -0,0 +1,19 @@
+/**
+ * Weather Skill
+ */
+var Skill = require('../../../src/Skill/skill');
+
+module.exports = class WeatherSkill extends Skill {
+
+/**
+ * Constructor
+ *
+ * @param App app
+ * @access public
+ * @return void
+ */
+	constructor(app) {
+		super(app);
+	}
+
+}
